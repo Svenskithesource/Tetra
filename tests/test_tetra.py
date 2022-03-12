841388cf8a7f17820a9a7db26609a3a29e93f7d3
svenskithesource <40274381+Svenskithesource@users.noreply.github.com>
2022-03-12 19:20:01 +0100

    Add more tests

--- tests/test_tetra.py
+++ tests/test_tetra.py
@@ -7,6 +7,23 @@ class TestTetra(unittest.TestCase):
 
     def test_div(self):
         self.assertEqual(tetra.Interpreter("1 / 2").run(), 0) # will be 0 because of floor division
+
+    def test_mul(self):
+        self.assertEqual(tetra.Interpreter("1 * 2").run(), 2)
+        self.assertEqual(tetra.Interpreter("1 * 2 * 3").run(), 6)
+    
+    def test_add(self):
+        self.assertEqual(tetra.Interpreter("1 + 2").run(), 3)
+        self.assertEqual(tetra.Interpreter("1 + 2 + 3").run(), 6)
+    
+    def test_sub(self):
+        self.assertEqual(tetra.Interpreter("1 - 2").run(), -1)
+        self.assertEqual(tetra.Interpreter("1 - 2 - 3").run(), -4)
+
     def test_paran(self):
         self.assertEqual(tetra.Interpreter("(1) + (2)").run(), 3)
         self.assertEqual(tetra.Interpreter("(1 + 2) * 3").run(), 9)
+    
+    def test_div_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            tetra.Interpreter("1 / 0").run()
